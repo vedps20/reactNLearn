e7be32133dc5eac0be0ac0cfc56ead5c5b0fce90
vedps20 <vedpratap9014@gmail.com>
2024-01-27 01:57:09 +0530

    adding Online status feature

---
+++ src/utils/useRestarauntMenu.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { MENU_API } from "./constants";
+
+
+export const useRestarauntMenu = (resId) =>{
+
+    const [resInfo, setResInfo] = useState(null);
+
+    useEffect(() => {
+        fetchMenu();
+    }, []);
+
+    const fetchMenu = async ()=> {
+        const data = await fetch(MENU_API + resId);
+        const json = await data.json();
+        setResInfo(json.data);
+    };
+
+    return resInfo;
+
+}
+
+
+// export default useRestarauntMenu;
